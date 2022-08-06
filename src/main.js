24530eafa014ad4a71f376e247d4ebb15f3d729e
aungkhantxenera <johnny.zaw@xenera.jp>
2022-08-07 01:10:20 +0630

    fixed

--- src/main.js
+++ src/main.js
@@ -4,6 +4,30 @@ import router from './router'
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui';
 import './utils/main.scss'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      // here I check that click was outside the el and his children
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  },
+});
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
--- src/views/Employees.vue
+++ src/views/Employees.vue
@@ -17,14 +17,30 @@
           </div>
         </div>
       </el-aside>
-      <el-container class="main">
-        <div>
-          <i class="el-icon-back"></i>
-          <h4>Employee Directory</h4>
+      <el-container class="main px-3 py-4">
+        <div class="d-flex flex-row align-items-center">
+          <i class="el-icon-back primary-text back-icon"></i>
+          <span class="ml-3 employee-directory-heading"
+            >Employee Directory</span
+          >
+          <div class="action-icons">
+            <i class="el-icon-search"></i>
+            <i class="el-icon-edit"></i>
+          </div>
         </div>
         <div>
-          <el-table class="employee-table" :data="tableData" style="width: 100%" >
-            <el-table-column fixed prop="Employees" label="Employees" width="150">
+          <el-table
+            class="employee-table"
+            :data="tableData"
+            style="width: 100%"
+          >
+            <el-table-column type="selection" width="55"> </el-table-column>
+            <el-table-column
+              fixed
+              prop="Employees"
+              label="Employees"
+              width="150"
+            >
             </el-table-column>
             <el-table-column prop="Location" label="Location" width="120">
             </el-table-column>
@@ -32,9 +48,36 @@
             </el-table-column>
             <el-table-column prop="Phone" label="Phone" width="120">
             </el-table-column>
-            <el-table-column prop="Department" label="Department" width="300">
+            <el-table-column label="Department" width="150">
+              <template slot-scope="scope">
+                <span
+                  v-show="selectedDeaprtment.id !== scope.row.id"
+                  @click="changeDepartment(scope.row.id)"
+                  >{{ scope.row.Department }}</span
+                >
+                <el-select
+                  v-show="selectedDeaprtment.id === scope.row.id"
+                  v-model="scope.row.Department"
+                  @blur="escSelect"
+                  @change="selectedDeaprtment.id = ''"
+                  filterable
+                  placeholder="Select"
+                >
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  >
+                  </el-option>
+                </el-select>
+              </template>
             </el-table-column>
-            <el-table-column prop="EmployeeCode" label="Employee Code" width="120">
+            <el-table-column
+              prop="EmployeeCode"
+              label="Employee Code"
+              width="150"
+            >
             </el-table-column>
             <el-table-column prop="CustomTags" label="Custom Tags" width="120">
             </el-table-column>
@@ -44,11 +87,23 @@
             </el-table-column>
             <el-table-column prop="Manager" label="Manager" width="120">
             </el-table-column>
-            <el-table-column prop="ManagerPosition" label="Manager Position" width="120">
+            <el-table-column
+              prop="ManagerPosition"
+              label="Manager Position"
+              width="150"
+            >
             </el-table-column>
-            <el-table-column prop="JoinnedDate" label="Joinned Date" width="120">
+            <el-table-column
+              prop="JoinnedDate"
+              label="Joinned Date"
+              width="120"
+            >
             </el-table-column>
-            <el-table-column prop="TerminateDate" label="Terminate Date" width="120">
+            <el-table-column
+              prop="TerminateDate"
+              label="Terminate Date"
+              width="150"
+            >
             </el-table-column>
           </el-table>
         </div>
@@ -59,7 +114,7 @@
 
 <script>
 import Sidebar from "../components/Sidebar.vue";
-import db from "../static/db.json"
+import db from "../static/db.json";
 
 export default {
   name: "EmployeePage",
@@ -72,10 +127,80 @@ export default {
         { label: "Departments structures" },
       ],
       tableData: db.employees,
+      multipleSelection: [],
+      counter: 0,
+      options: [
+        {
+          value: "Quality assurance engineering YGN Department",
+          label: "Quality assurance engineering YGN Department",
+        },
+        {
+          value: "Development",
+          label: "Development",
+        },
+        {
+          value: "product",
+          label: "product",
+        },
+        {
+          value: "Customer success",
+          label: "Customer success",
+        },
+        {
+          value: "Marketing Myanmar department",
+          label: "Marketing Myanmar department",
+        },
+        {
+          value: "Marketing Srilanka department",
+          label: "Marketing Srilanka department",
+        },
+        {
+          value: "Human resource managing department",
+          label: "Human resource managing department",
+        },
+      ],
+      selectedDeaprtment: {
+        id: "",
+      },
     };
   },
   components: {
     Sidebar,
+  },
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach((row) => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    changeDepartment(id) {
+      this.counter++;
+
+      if (this.counter == 1) {
+        this.timer = setTimeout(() => {
+          this.counter = 0;
+          // alert('double click'+id)
+          // window.location.href = 'p/'+ postId;
+        }, 300);
+
+        return;
+      }
+
+      clearTimeout(this.timer);
+      // alert('this is real click'+id)
+      this.selectedDeaprtment.id = id;
+      this.counter = 0;
+    },
+    escSelect() {
+      this.selectedDeaprtment.id = "";
+    },
   },
 };
 </script>
@@ -101,5 +226,20 @@ export default {
 }
 .employee-table {
   border-bottom: 0px;
+}
+.back-icon {
+  font-size: 25px;
+  font-weight: 900;
+  margin-right: 10px;
+  cursor: pointer;
+}
+.employee-directory-heading {
+  font-size: 24px;
+}
+.action-icons {
+  margin-left: 10px;
+}
+.action-icons > i {
+  font-weight: 900;
 }
 </style>
